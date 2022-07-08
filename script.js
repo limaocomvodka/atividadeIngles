327b3925b38226e73e0465c85a487e5acde38164
limaocomvodka <104787948+limaocomvodka@users.noreply.github.com>
2022-07-08 17:38:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+//Crie 3 variáveis, cada uma com um array:
+
+let comedy = [
+    {title: "O Golpista do Ano",
+    director:"Glenn Ficarra",
+    year:2009
+},
+    {title: "Ace Ventura",
+   director: "Tom Shadyac",
+   year: 1994
+   },
+   {title: "Todo Mundo em Pânico 2",
+   director: "Keenen Wayans",
+   year: 2004
+   }
+]
+console.log (comedy);
+let action = [
+    {title: "Cão de Briga",
+    director: "Louis Leterrier",
+     year: 2005
+    },
+    {title: "Dia de Treinamento",
+    director: "Antoine Fuqua",
+    year: 2002
+    },
+    {title: "Reis da Rua",
+    director: "David Ayer",
+    year: 2008
+    }
+]
+console.log (action);
+let fantasy = [
+    {title:"O Senhor dos Anéis: A Sociedade do Anel",
+    director:"Peter Jackson",
+    year: 2001
+    },
+    {
+    title:"O Senhor dos Anéis: As Duas Torres",
+    director:"Peter Jackson",
+    year: 2002
+    },
+    {
+    title:"O Senhor dos Anéis: O Retorno do Rei",
+    director:"Peter Jackson",
+    year: 2003
+    }
+    ]
+    console.log(fantasy);
+
+
+//*DESAFIO EXTRA* 
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+//IF: "MY FAVORIT MOVIE IS..."
+//ELSE: "NOT EVEN 5 STARS..."
+
